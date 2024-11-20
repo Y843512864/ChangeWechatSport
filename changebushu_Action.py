952bd950c814ab6e847ae4edad4c0e7d049889db
Y843512864 <843512864@QQ.COM>
2024-11-20 10:01:11 +0800

    Update changebushu_Action.py
    mod

--- changebushu_Action.py
+++ changebushu_Action.py
@@ -125,4 +125,5 @@ if __name__ == "__main__":
     step = str(randint(int(os.environ['STEP_MIN']), int(os.environ['STEP_MAX'])))
     # step = os.environ['STEP']
     # step = str(randint(10123, 12302)) 
+    print('user:{},step:{}'.format(user, step))
     main()
