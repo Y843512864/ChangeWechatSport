7ad6e0302424d49fa9593b681a3039174ad425b9
dedong.yin <dedong.yin@pactera.com>
2024-11-20 09:58:50 +0800

    mod

--- changebushu_Action.py
+++ changebushu_Action.py
@@ -123,6 +123,7 @@ if __name__ == "__main__":
     user = os.environ['USER_PHONE']
     password = os.environ['USER_PWD']
     step = str(randint(int(os.environ['STEP_MIN']), int(os.environ['STEP_MAX'])))
+    print('user:{},step:{}'.format(user, step))
     # step = os.environ['STEP']
     # step = str(randint(10123, 12302)) 
     main()
